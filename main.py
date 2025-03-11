250f8b3bffbdc1fd2b9b175cd5c4b3b861cc1022
DanGossDev <dangossfamily@gmail.com>
2025-03-10 21:21:56 -0400

    Updating bookbot lesson 2-6

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ def get_book_text(path):
 
 from stats import wordcount
 
+from stats import charcount
+
 def main():
     words = get_book_text("./books/frankenstein.txt")
     print(f"{wordcount(words)} words found in the document")
+    print(charcount(words))
 
 main()
 
--- stats.py
+++ stats.py
@@ -2,5 +2,10 @@ def wordcount(contents):
     return len(contents.split())
 
 def charcount(contents):
-    countlib = {}
-    
+    countlib_dict = {}
+    characters = [char for char in contents]
+    for character in characters:
+        if character not in countlib_dict:
+            countlib_dict[character.lower()] = 0
+        countlib_dict[character.lower()] = countlib_dict[character] + 1
+    return countlib_dict
